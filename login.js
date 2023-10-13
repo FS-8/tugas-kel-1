c262e6d5a01b2fcccc0896c911f54c771e35db7a
adtya27 <adityanfirdaus@gmail.com>
2023-10-13 20:00:26 +0700

    styling

---
+++ login.js
@@ -0,0 +1,17 @@
+// Login
+// const username = document.forms['login-form']['username'];
+// const password = document.forms['login-form']['password'];
+
+// username.addEventListener('textinput', username_Verify);
+// password.addEventListener('textinput', pass_Verify);
+
+// function username_Verify() {
+//   if (username.value.length >= 8) {
+//     return true;
+//   }
+// }
+// function pass_Verify() {
+//   if (password.value.length >= 5) {
+//     return true;
+//   }
+// }
