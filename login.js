3c3ef8b253b77df57f9a01469c0fff6d033f8251
adtya27 <adityanfirdaus@gmail.com>
2023-10-18 21:38:42 +0700

    input login

--- login.js
+++ login.js
@@ -1,17 +1,49 @@
-// Login
-// const username = document.forms['login-form']['username'];
-// const password = document.forms['login-form']['password'];
+function login(event) {
+  event.preventDefault();
 
-// username.addEventListener('textinput', username_Verify);
-// password.addEventListener('textinput', pass_Verify);
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('pass').value;
 
-// function username_Verify() {
-//   if (username.value.length >= 8) {
-//     return true;
-//   }
+  const url = new URL('https://652c0692d0d1df5273ef0d3c.mockapi.io/v1/login');
+  url.searchParams.append('username', username);
+  // url.searchParams.append('password', password);
+
+  // console.log(url);
+
+  fetch(url, {
+    method: 'GET',
+    headers: { 'content-type': 'application/json' },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      if (data.length > 0 && validateLogin(username, password, data[0])) {
+        console.log(data[0]);
+        window.location.replace('index.html');
+      } else {
+        console.log('pass salah');
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// function test() {
+// location.replace('index.html');
 // }
-// function pass_Verify() {
-//   if (password.value.length >= 5) {
-//     return true;
-//   }
-// }
+
+function validateLogin(username, password, data) {
+  if (username != data.username) {
+    return false;
+  }
+
+  if (password != data.password) {
+    return false;
+  }
+
+  return true;
+}
