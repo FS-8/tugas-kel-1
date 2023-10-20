cec6d49a17806ef3185f466a0fce68fad9b274ee
farid <valkanid12@gmail.com>
2023-10-20 16:14:12 +0700

    perbaikan loop produk

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-var item = document.querySelectorAll(".carousel .carousel-item");
-item.forEach((e) => {
-  const slide = 4;
-  let next = e.nextElementSibling;
-  for (var i; i < slide; i++) {
-    if (!next) {
-      next = item[0];
-    }
-    let clonechild = next.cloneNode(true);
-    e.appendChild(clonechild.children[0]);
-    next = next.nextElementSibling;
-  }
-});
-
 let listSepatu = document.getElementById("list-produk");
-
+// loop produk
 async function getDataSepatu() {
   try {
     let respons = await fetch("https://652d214cf9afa8ef4b26d419.mockapi.io/sepatu");
@@ -24,7 +10,7 @@ async function getDataSepatu() {
     data.map((item, index) => {
       let dataSepatu = `
         <div class="card m-2" style="width: 18rem;">
-        <img src="${item.img}.jpg" class="card-img-top" alt="...">
+        <img src="${item.img}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${item.nama}</h5>
           <h6 class="card-title">Rp.${item.harga}</h6>
@@ -43,26 +29,26 @@ async function getDataSepatu() {
 
 getDataSepatu();
 
-// async function getDataCarousel() {
-//   let respons = await fetch("https://652d214cf9afa8ef4b26d419.mockapi.io/sepatu");
-//   let data = await respons.json();
-//   data.map((item, index) => {
-//     let dataCarousel = `
-//       <div class="carousel-item row d-flex align-items-center justify-content-center ">
-//       <div class="card" style="width: 18rem;">
-//         <div class="img-wrapper">
-//           <img src="${item.img}.jpg"  alt="...">
-//         </div>
-//         <div class="card-body">
-//           <h5 class="card-title">${item.nama}</h5>
-//           <h6 class="card-title">Rp.${item.harga}</h6>
-//           <p class="card-text">${item.tag}</p>
-//           <a href="#" class="btn btn-produk">Beli</a>
-//         </div>
-//       </div>
-//     </div>`;
-//     listSepatu.innerHTML += dataSepatu;
-//   });
-// }
+// loop home
+let car1 = document.getElementById("pter");
 
-// getDataCarousel();
+async function getDataCarousel1() {
+  let respons = await fetch("https://652d214cf9afa8ef4b26d419.mockapi.io/sepatu");
+  let data = await respons.json();
+  data.slice(0, 4).map((item, index) => {
+    let dataSepatu = `
+      <div class="card m-2" style="width: 18rem;">
+      <img src="${item.img}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${item.nama}</h5>
+        <h6 class="card-title">Rp.${item.harga}</h6>
+        <p class="card-text">${item.tag}</p>
+        <a href="#" class="btn btn-produk">Beli</a>
+      </div>
+    </div>
+      `;
+    car1.innerHTML += dataSepatu;
+  });
+}
+
+getDataCarousel1();
